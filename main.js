dc3e91d708c51417548166a31a35e482360b4b1a
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-01-15 18:07:08 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,9 @@ function modelLoaded()
 {
     console.log('Model Loaded');
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
